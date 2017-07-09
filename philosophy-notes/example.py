d10703427947fd34e20dd6937fa164f87ea17c59
Michael Burgess <michael.burgess@qa.com>
2017-07-09 19:55:13 +0100

    update

---
+++ philosophy-notes/example.py
@@ -0,0 +1,28 @@
+
+
+class Car:
+	WHEELS = 4
+
+	def __init__(self, fuel):
+		self.fuel = fuel
+
+
+my_car = Car(100)
+
+"""
+
+There are a class (group) of objects in the problem domain called 'Car's.
+
+Every Car has:
+	4 WHEELS
+	
+Every Car can be made by supplying:
+	fuel
+
+Making a Car consists of creating a Car object 
+	which stores the fuel which was supplied. 
+
+
+my_car is the name of a Car object whose fuel is 100.
+
+"""
